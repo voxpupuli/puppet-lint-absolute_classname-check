7974217326513c78379ecee000741bf1abf2a4b0
Matthias Baur <m.baur@syseleven.de>
2019-01-14 15:56:05 +0100

    Add option to reverse the check
    Meaning: Warn if there are :: in front of a class definition.
    
    Fixes #17.

--- lib/puppet-lint/plugins/check_absolute_classname.rb
+++ lib/puppet-lint/plugins/check_absolute_classname.rb
@@ -1,10 +1,19 @@
 PuppetLint.new_check(:relative_classname_inclusion) do
   def check
     tokens.each_with_index do |token, token_idx|
+      reverse = PuppetLint.configuration.absolute_classname_reverse || false
+      if reverse
+        pattern = '^(?!::)'
+        message = 'class included by absolute name (::$class)'
+      else
+        pattern = '^::'
+        message = 'class included by relative name'
+      end
       if [:NAME,:FUNCTION_NAME].include?(token.type) && ['include','contain','require'].include?(token.value)
         s = token.next_code_token
         next if s.nil?
         next if s.type == :FARROW
+
         in_function = 0
         while s.type != :NEWLINE
           n = s.next_code_token
@@ -13,12 +22,12 @@ PuppetLint.new_check(:relative_classname_inclusion) do
               in_function += 1
             elsif in_function > 0 && n && n.type == :RPAREN
               in_function -= 1
-            elsif in_function == 0 && s.value !~ /^::/
+            elsif in_function.zero? && s.value !~ /#{pattern}/
               notify :warning, {
-                :message => 'class included by relative name',
-                :line    => s.line,
-                :column  => s.column,
-                :token   => s,
+                message: message,
+                line: s.line,
+                column: s.column,
+                token: s
               }
             end
           end
@@ -27,21 +36,26 @@ PuppetLint.new_check(:relative_classname_inclusion) do
       elsif token.type == :CLASS and token.next_code_token.type == :LBRACE
         s = token.next_code_token
         while s.type != :COLON
-          if (s.type == :NAME || s.type == :SSTRING) && s.value !~ /^::/
+          if (s.type == :NAME || s.type == :SSTRING) && s.value !~ /#{pattern}/
             notify :warning, {
-              :message => 'class included by relative name',
-              :line    => s.line,
-              :column  => s.column,
-              :token   => s,
+              message: message,
+              line: s.line,
+              column: s.column,
+              token: s
             }
           end
           s = s.next_token
         end
       end
     end
-  end 
+  end
 
   def fix(problem)
-    problem[:token].value = '::'+problem[:token].value
+    reverse = PuppetLint.configuration.absolute_classname_reverse || false
+    problem[:token].value = if reverse
+                              problem[:token].value[2..-1]
+                            else
+                              '::' + problem[:token].value
+                            end
   end
 end
